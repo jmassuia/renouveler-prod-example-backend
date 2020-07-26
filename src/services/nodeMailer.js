786af2f74e72488039fea178643d3c414a725fc8
jmassuia <massuia1507@gmail.com>
2020-07-25 22:28:55 -0300

    changing .env references

--- src/services/nodeMailer.js
+++ src/services/nodeMailer.js
@@ -9,10 +9,10 @@ const transporter = nodemailer.createTransport({
   auth: {
       type:"OAuth2",
       user: "massuia1507@gmail.com",
-      clientId:process.env.GMAIL_CLIENT_ID2/*'521860514801-0dm90f5d4qblga2v37pnrqcsa4hn9r3a.apps.googleusercontent.com'*/,
-      clientSecret:process.env.GMAIL_CLIENT_SECRET2/*'SvAfdzJHlQ3z1WitY76wPpGf'*/,
-      refreshToken:process.env.GMAIL_REFRESH_TOKEN2/*'1//04rz9RGlxIwFTCgYIARAAGAQSNwF-L9IrUcCciuR6cpEWnFEYQBC-OSIPDvaJQ2aroBdEL-lyFAN5TxAM139GfoWZaHiVUYiW6fU'*/,
-      accessToken:process.env.GMAIL_ACCESS_TOKEN2/* 'ya29.Il-9B5f29PQb9BzDkW7mTG3mZR2C_RmAb8iwi_WZHZ7X3hjJ-uX0V4_QkJV1nOzrh9MqlLnIP-hfeLWWaa85fWzovq-SdR_SdkaUWSF-9MJDht2RAG_c0xyAehx8eM02OA'*/,
+      clientId:process.env.GMAIL_CLIENT_ID/*'521860514801-0dm90f5d4qblga2v37pnrqcsa4hn9r3a.apps.googleusercontent.com'*/,
+      clientSecret:process.env.GMAIL_CLIENT_SECRET/*'SvAfdzJHlQ3z1WitY76wPpGf'*/,
+      refreshToken:process.env.GMAIL_REFRESH_TOKEN/*'1//04rz9RGlxIwFTCgYIARAAGAQSNwF-L9IrUcCciuR6cpEWnFEYQBC-OSIPDvaJQ2aroBdEL-lyFAN5TxAM139GfoWZaHiVUYiW6fU'*/,
+      accessToken:process.env.GMAIL_ACCESS_TOKEN/* 'ya29.Il-9B5f29PQb9BzDkW7mTG3mZR2C_RmAb8iwi_WZHZ7X3hjJ-uX0V4_QkJV1nOzrh9MqlLnIP-hfeLWWaa85fWzovq-SdR_SdkaUWSF-9MJDht2RAG_c0xyAehx8eM02OA'*/,
       expires: 2500
   }
 });
