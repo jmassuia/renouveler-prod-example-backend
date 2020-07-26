0af7a844ac8ad9072c8720422429b0f7a1dd63bf
jmassuia <massuia1507@gmail.com>
2020-07-25 21:22:19 -0300

    changing root return message

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ const routes = express.Router();
 const clientController = require('./controller/clientController');
 
 routes.get('/',(req,res)=>{
-   return res.json('ok');
+   return res.json('Application is running!!!');
 })
 
 routes.post('/sendMail',clientController.store);
